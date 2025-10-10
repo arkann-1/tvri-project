50c232ac08a956637d3a69528f30602936dd826e
arkann-1 <okanjiki002@gmail.com>
2025-10-10 19:31:01 +0700

    sidebar dan topbar editjadwal.php

--- pages/editjadwal.php
+++ pages/editjadwal.php
@@ -20,10 +20,39 @@ if (!$data) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Edit Jadwal</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body class="bg-light">
-<div class="container mt-5">
+<body>
+<!-- Sidebar -->
+  <aside class="sidebar" id="sidebar">
+    <div class="d-flex align-items-center justify-content-between px-3 mb-3">
+      <div class="d-flex align-items-center gap-2">
+        <button id="pinSidebar" class="pin-btn" title="Collapse/Expand">📌</button>
+        <h4 class="m-0 ms-3 text-light">MENU</h4>
+      </div>
+    </div>
+    <a href="../index.php">🏠 <span class="menu-text">Beranda</span></a>
+    <a href="jadwalbulanan.php">📅 <span class="menu-text">Jadwal Bulanan</span></a>
+    <a href="rekap.php">✉️ <span class="menu-text">Rekap</span></a>
+    <a href="tambahpegawai.php">➕ <span class="menu-text">Tambah Pegawai</a>
+    <a href="tambahjadwal_otomatis.php">➕ <span class="menu-text">Tambah Jadwal</a>
+    <a href="/tvri-project/auth/logout.php" class="mt-auto">⚠️ <span class="menu-text">Logout</span></a>
+  </aside>
+
+<!-- Main -->
+<main class="main">
+  <!-- Topbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm rounded mb-4">
+      <div class="container-fluid">
+        <a class="navbar-brand mx-auto" href="#">
+          <img src="../assets/images/TVRI.png" alt="TVRI" width="90" class="d-block mx-auto">
+        </a>
+       </div>
+     </nav> 
+
+<div class="card">
   <div class="card shadow">
     <div class="card-header bg-warning text-dark">
       <h4 class="m-0">✏️ Edit Jadwal</h4>
@@ -97,5 +126,7 @@ if (!$data) {
     </div>
   </div>
 </div>
+  
+  <script src="../assets/js/script.js"></script>
 </body>
 </html>
